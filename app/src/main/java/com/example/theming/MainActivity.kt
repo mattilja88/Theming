2458ef8bf7a0a7d12ff7d61f70c20e5bb3e6f435
Jaakko Mattila <t3maja06@students.oamk.fi>
2024-09-24 13:37:01 +0300

    walkthrough Theming

---
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.theming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.theming.ui.theme.MyTheme
+import com.example.theming.ui.theme.ThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "My Title",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = appModifier.padding(top = 24.dp)
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = appModifier
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = appModifier
+        ) {
+            Text(text = "Submit")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyTheme {
+        MyApp()
+    }
+}
